925eb3638f9785ed98bba14f48e2c868649ef9de
Harald Grove <haraldgrove@gmail.com>
2018-02-16 15:54:52 +0700

    Converted to class based structure.

---
+++ transpose_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+infile = sys.argv[1]
+i1, i2 = infile.rsplit('.',1)
+outfile = '{}_T.{}'.format(i1,i2)
+db = []
+with open(infile, 'r') as fin:
+    for line in fin:
+        l = line.strip().split()
+        db.append(l[3:])
+with open(outfile, 'w') as fout:
+    for line in zip(*db):
+        fout.write('{}\n'.format('\t'.join(line)))
