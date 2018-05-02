02bd6013097c2a7a4ec74bf8de90bb1815b69177
Harald Grove <haraldgrove@gmail.com>
2018-05-02 12:49:13 +0700

    Added bedfile as possible input.

--- grepfasta.py
+++ grepfasta.py
@@ -12,6 +12,7 @@ __license__ = "MIT"
 import argparse
 import time
 import sys
+import os.path
 import gzip
 
 
@@ -78,13 +79,75 @@ def getpartialfasta(fastafile, pattern, start, end, reverse):
                     sys.stdout.write('{}\n'.format(seq[start:end][::-1].translate(trans)))
 
 
+def getbedfasta(fastafile, bedfile):
+    """
+    Sends sequence selection of any sequence with name matching pattern to stdout
+    :param fastafile: Input fastafile, plain text
+    :param bedfile: Name of bedfile with sequence coordinates
+    :return:
+    """
+    bed = {}
+    with open(bedfile, 'r') as fin:
+        for line in fin:
+            l = line.strip().split()
+            if len(l) == 6:
+                chrom, start, end, name, score, strand = l
+            elif len(l) == 4:
+                chrom, start, end, name = l
+                strand = '+'
+            elif len(l) == 3:
+                chrom, start, end = l
+                name = '{}:{}-{}'.format(chrom, start, end)
+                strand = '+'
+            else:
+                continue
+            if chrom not in bed:
+                bed[chrom] = []
+            bed[chrom].append((name, int(start), int(end), strand))
+    trans = str.maketrans('ACGTacgt', 'TGCAtgca')
+    active = False
+    if fastafile[-3:] == '.gz':
+        op = gzip.open
+    else:
+        op = open
+    seq = ''
+    with op(fastafile, 'rt') as fin:
+        for line in fin:
+            if line.startswith('>'):
+                if active:
+                    for (name, start, end, strand) in bed[chrom]:
+                        if strand == '+':
+                            sys.stdout.write('>{}\n{}\n'.format(name, seq[start:end]))
+                        else:
+                            sys.stdout.write('>{}\n{}\n'.format(name, seq[start:end][::-1].translate(trans)))
+                    seq = ''
+                chrom = line[1:].strip().split()[0]
+                if chrom in bed:
+                    active = True
+                else:
+                    active = False
+            elif active:
+                seq += line.strip()
+        else:
+            if active:
+                for (name, start, end, strand) in bed[chrom]:
+                    if strand == '+':
+                        sys.stdout.write('>{}\n{}\n'.format(name, seq[start:end]))
+                    else:
+                        sys.stdout.write('>{}\n{}\n'.format(name, seq[start:end][::-1].translate(trans)))
+
+
+
 def main(args):
     """ Main entry point of the app """
-    if args.coordinates == "0,0":
-        getfasta(args.fastafile, args.pattern, args.reverse)
+    if os.path.isfile(args.pattern):
+        getbedfasta(args.fastafile, args.pattern)
     else:
-        s,e = [int(c) for c in args.coordinates.split(",")]
-        getpartialfasta(args.fastafile, args.pattern, s-1, e, args.reverse)
+        if args.coordinates == "0,0":
+            getfasta(args.fastafile, args.pattern, args.reverse)
+        else:
+            s,e = [int(c) for c in args.coordinates.split(",")]
+            getpartialfasta(args.fastafile, args.pattern, s-1, e, args.reverse)
     if args.log:
         with open('README.txt', 'a') as fout:
             fout.write('[{}]\t[{}]\n'.format(time.asctime(), ' '.join(sys.argv)))
@@ -95,7 +158,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Required positional argument
-    parser.add_argument("pattern", help="Search pattern")
+    parser.add_argument("pattern", help="Search pattern or bed-file")
     parser.add_argument("fastafile", help="Input fasta file")
 
     # Optional argument flag which defaults to False
